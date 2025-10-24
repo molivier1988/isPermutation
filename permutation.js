19753ad7192e025599cbbb9dfb6c316c127853d7
molivier1988 <markolivier1969@gmail.com>
2025-10-24 16:47:55 +0100

    Modified comments section of permutation.js

--- permutation.js
+++ permutation.js
@@ -12,4 +12,4 @@ The actual solution (above) was much more elegant.
 Step 1. Use the split('') method to convert the string into an array of single characters. Do this for both arguments.
 Step 2. Sort both arrays, when sorted both arrays should contain the same characters in the same order (if perumations)
 Step 4. Join both arrays into strings with join()
-Step 5. We can now use the equality operator to test is both strings are the same.
+Step 5. We can now use the equality operator to test is both strings are the same.*/
